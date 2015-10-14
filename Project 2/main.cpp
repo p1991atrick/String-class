9482c149e2ab65c65d119095549e28a6fd7192f7
Patrick <p1991atrick@gmail.com>
2015-10-13 19:10:50 -0700

    Initial Commit

---
+++ Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 2
+//
+//  Created by Gordon Freeman on 10/13/15.
+//  Copyright © 2015 Patrick Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
